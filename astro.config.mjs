c29701e0249d280aa998fa4781d05fa466bab83c
Will <potatoboy9999@outlook.com>
2025-01-11 14:38:14 -0800

    Switch to github pages hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,13 +7,11 @@ import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
 
-import vercel from "@astrojs/vercel";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind({
     applyBaseStyles: false,
   }), mdx()],
 
-  adapter: vercel(),
+  site: 'https://gdsc-oregonstate.github.io/'
 });
